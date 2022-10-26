fd4a88235bae233651af6da87e82826d853768dd
Rafael <rafaelf.machado2004@gmail.com>
2022-10-26 14:20:05 -0300

    Cadastro de clientes

---
+++ templates/static/clientes/js/clientes.js
@@ -0,0 +1,10 @@
+function add_carro(){
+
+    container = document.getElementById('form-carro')
+
+    html = "<br>  <div class='row'> <div class='col-md'> <input type='text' placeholder='Carro' class='form-control' name='carro' > </div> <div class='col-md'><input type='text' placeholder='Placa' class='form-control' name='placa' ></div> <div class='col-md'> <input type='number' placeholder='Ano' class='form-control' name='ano'> </div> </div>"
+
+    container.innerHTML += html
+
+
+}
