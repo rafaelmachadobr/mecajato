5d4f0bbde2075847821fc69da1d87f4208a1a4cc
Rafael <rafaelf.machado2004@gmail.com>
2023-01-10 14:48:29 -0300

    Cadastro de serviços

--- servicos/views.py
+++ servicos/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
-
+from .forms import FormServico
+from django.http import HttpResponse
 
 def novo_servico(request):
-    return render(request, 'novo_servico.html')
+    if request.method == "GET":
+        form = FormServico()
+        return render(request, 'novo_servico.html', {'form': form})
+    elif request.method == "POST":
+        form = FormServico(request.POST)
+
+        if not form.is_valid():
+            return render(request, 'novo_servico.html', {'form': form})
+        form.save()
+        return HttpResponse('Salvo com sucesso')
