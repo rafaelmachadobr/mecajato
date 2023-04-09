15577c110974e0f291e05eaf08c8be1142bc4e4e
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-04-09 11:13:22 -0300

    Listagem de serviços

--- servicos/views.py
+++ servicos/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .forms import FormServico
 from django.http import HttpResponse
+from .models import Servico
+
 
 def novo_servico(request):
     if request.method == "GET":
@@ -13,3 +15,15 @@ def novo_servico(request):
             return render(request, 'novo_servico.html', {'form': form})
         form.save()
         return HttpResponse('Salvo com sucesso')
+
+
+def listar_servico(request):
+    if request.method == "GET":
+        servicos = Servico.objects.all()
+        return render(request, 'listar_servico.html', {'servicos': servicos})
+
+
+def servico(request, identificador):
+    if request.method == "GET":
+        servico = get_object_or_404(Servico, identificador=identificador)
+        return render(request, 'servico.html', {'servico': servico})
